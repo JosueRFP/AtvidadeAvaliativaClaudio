22dfc2a081c5b0a9330067e4002ea2726602f91a
JosueRFP <josue.petri.senac@gmail.com>
2024-05-07 16:26:09 -0300

    Andar do player
    Fiz script para mov do player

--- AtvidadeAvaliativaClaudio/Assets/Scripts/Player.cs
+++ AtvidadeAvaliativaClaudio/Assets/Scripts/Player.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int speed = 5 , jump = 4;
+    float speed = 5;
+    public Rigidbody2D body;
+    float horizontal;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontal * speed, body.velocity.y);
     }
 
 }
